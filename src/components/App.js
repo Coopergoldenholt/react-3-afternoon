5916ac8ce5f9c2c5d4bad526e05b0b46c1d77761
coopergoldenholt <coopergoldenholt12@gmail.com>
2019-12-11 10:05:37 -0700

    finished project

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,99 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import Post from "./Post/Post";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      posts: []
-    };
+		this.state = {
+			posts: []
+		};
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
-  }
-  
-  componentDidMount() {
+		this.updatePost = this.updatePost.bind(this);
+		this.deletePost = this.deletePost.bind(this);
+		this.createPost = this.createPost.bind(this);
+		this.searchPost = this.searchPost.bind(this);
+	}
 
-  }
+	componentDidMount() {
+		axios.get(`https://practiceapi.devmountain.com/api/posts`).then(res => {
+			this.setState({
+				posts: res.data
+			});
+		});
+	}
 
-  updatePost() {
-  
-  }
+	updatePost(id, text) {
+		axios
+			.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+			.then(res => {
+				console.log(res.data);
+				this.setState({
+					posts: res.data
+				});
+			});
+	}
 
-  deletePost() {
+	deletePost(id) {
+		axios
+			.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+			.then(res => {
+				this.setState({
+					posts: res.data
+				});
+			});
+	}
 
-  }
+	createPost(text) {
+		axios
+			.post(`https://practiceapi.devmountain.com/api/posts`, { text })
+			.then(res => {
+				this.setState({
+					posts: res.data
+				});
+			});
+	}
 
-  createPost() {
+	searchPost(text) {
+		axios.get(`https://practiceapi.devmountain.com/api/posts`).then(res => {
+			const filterPosts = res.data.filter(element => {
+				return element.text.includes(text);
+			});
+			this.setState({
+				posts: filterPosts
+			});
+		});
+	}
 
-  }
+	render() {
+		const { posts } = this.state;
 
-  render() {
-    const { posts } = this.state;
+		return (
+			<div className="App__parent">
+				<Header searchPostFn={this.searchPost} />
 
-    return (
-      <div className="App__parent">
-        <Header />
-
-        <section className="App__content">
-
-          <Compose />
-          
-        </section>
-      </div>
-    );
-  }
+				<section className="App__content">
+					<Compose createPostFn={this.createPost} />
+					{posts.map(post => (
+						<Post
+							key={post.id}
+							text={post.text}
+							date={post.date}
+							updatePostFn={this.updatePost}
+							id={post.id}
+							deletePostFn={this.deletePost}
+						/>
+					))}
+				</section>
+			</div>
+		);
+	}
 }
 
 export default App;
